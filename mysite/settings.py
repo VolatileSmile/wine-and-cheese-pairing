26e4a201b829267e813c4f132f2c0a9b33035011
Ali Woo <alistairwoo2@gmail.com>
2022-03-18 15:11:53 +0800

    18/3 settings.py middlware 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
