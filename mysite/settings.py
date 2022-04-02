678d3d9826f807ec7f2a2e8b9cd796052ef0c94a
Ali Woo <alistairwoo2@gmail.com>
2022-04-02 21:26:34 +0800

    ali

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'crispy_forms',
     'polls.apps.PollsConfig',
-    'django_heroku',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
